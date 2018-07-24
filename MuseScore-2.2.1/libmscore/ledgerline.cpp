3f1db0e74f2c238cdc1b790290204555cc13cc8e
eaguirre_bewave <emilio.aguirre@bewave.io>
2018-07-24 16:16:16 -0400

    Fix elargisement lignes Do

--- MuseScore-2.2.1/libmscore/ledgerline.cpp
+++ MuseScore-2.2.1/libmscore/ledgerline.cpp
@@ -83,9 +83,9 @@ void LedgerLine::draw(QPainter* painter) const
         qreal sp = spatium();
         qreal vLineStart = sp * 0.35;
         qreal vLineEnd = sp * 0.65;
-        qreal ledW = 2.5 * sp; // 2.0 * sp;
-        qreal hLine1 = ledW * 0.25; // 0.08
-        qreal hLine2 = ledW * 0.75; // 0.92
+        qreal ledW = 2.0 * sp;
+        qreal hLine1 = ledW * 0.08;
+        qreal hLine2 = ledW * 0.92;
         float lw = score()->styleS(StyleIdx::staffLineWidth).val() * sp;
         int line = LineId();
         int pitch = line2pitch(line,ClefType::UTPIANO_RIGHT,Key::C);
@@ -112,7 +112,7 @@ void LedgerLine::draw(QPainter* painter) const
             qreal z = 2.0*sp;
             painter->setBrush(QBrush(QColor(200, 200, 200)));
             painter->setPen(Qt::NoPen);
-            painter->drawRect(QRect(-lw, -z, ledW+(lw*2), sp));
+            painter->drawRect(QRect(-lw, -z, ledW+lw, sp));
             // Black line
             painter->setPen(QPen(QColor(0, 0, 0), lw, Qt::SolidLine, Qt::SquareCap));
             painter->drawLine(QLineF(0,  -z, ledW,  -z));
@@ -131,7 +131,7 @@ void LedgerLine::draw(QPainter* painter) const
             // Gray box
             painter->setBrush(QBrush(QColor(200, 200, 200)));
             painter->setPen(Qt::NoPen);
-            painter->drawRect(QRect(-lw, 0, ledW+(lw*2), sp));
+            painter->drawRect(QRect(-lw, 0, ledW+lw, sp));
             // Drawing marks indicating C
             if (isCSharp)
             {
